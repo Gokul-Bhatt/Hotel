a6f2b468af415e4156a0f7ab4f3d35ad7ddf7200
gokul <142083386+Gokul-Bhatt@users.noreply.github.com>
2025-04-05 13:17:10 +0530

    footer

--- frontend/src/components/Home/footer.jsx
+++ frontend/src/components/Home/footer.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React from "react";
 import "./home.css";
+import icon from "../assets/mainimg.jpeg";
+import Insta from "../assets/insta.jpg";
+import Fb from "../assets/fb.png"
 
 const footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className="footers">
+      <div className="footerabout">
+        <img src={icon} alt="" className="iconn"/>
+        <p className="twoline">
+          Jageshwar Inn – Your Comfort Stay in the Heart of Jageshwar Experience
+          peace, nature, and heritage just steps away from the sacred Jageshwar
+          temples. Book your stay today and feel at home amidst the Himalayas.
+        </p>
+        <div className="igfbcl">
+        <a href="https://www.instagram.com/"><img src={Insta} alt="" className="igfb"/></a>
+        <a href="https://www.facebook.com/"><img src={Fb} alt=""  className="igfb"/></a>
+        </div>
+      </div>
+      <div className="footercontact">
+        <h1>OUR LOCATION</h1>
+        <p>Hotel Jageshwar In , Jageshwar, Almora-263623</p>
+        <p>book@gmail.com</p>
+        <p>9898989898</p>
+      </div>
+      <div className="lastfooter">
+        @2025 Hotel Jageshwar In, Jageshwar, Almora-263623
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
